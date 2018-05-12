78668ab42cce9005dc6a58dba7ff5c5f8eac99a3
gaiar <gaiar@baimuratov.ru>
2018-05-12 19:25:03 +0200

    Added tensorflow path for RPI 3

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,23 +2,14 @@
 
 # install using pip from the whl files on PyPI
 
-if [ `uname` == Darwin ]; then
-    if [ "$PY_VER" == "2.7" ]; then
-        WHL_FILE=https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-${PKG_VERSION}-py2-none-any.whl
-    else
-        WHL_FILE=https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-${PKG_VERSION}-py3-none-any.whl
-    fi
-fi
+#https://github.com/lhelontra/tensorflow-on-arm/releases/download/v1.8.0/tensorflow-1.8.0-cp35-none-linux_armv7l.whl
+#https://github.com/lhelontra/tensorflow-on-arm/releases/download/v1.8.0/tensorflow-1.8.0-cp27-none-linux_armv7l.whl
 
 if [ `uname` == Linux ]; then
     if [ "$PY_VER" == "2.7" ]; then
-        WHL_FILE=https://pypi.org/packages/cp27/t/tensorflow/tensorflow-${PKG_VERSION}-cp27-cp27mu-manylinux1_x86_64.whl
-    elif [ "$PY_VER" == "3.4" ]; then
-        WHL_FILE=https://pypi.org/packages/cp34/t/tensorflow/tensorflow-${PKG_VERSION}-cp34-cp34m-manylinux1_x86_64.whl
+        WHL_FILE=https://github.com/lhelontra/tensorflow-on-arm/releases/download/v${PKG_VERSION}/tensorflow-${PKG_VERSION}-cp27-none-linux_armv7l.whl
     elif [ "$PY_VER" == "3.5" ]; then
-        WHL_FILE=https://pypi.org/packages/cp35/t/tensorflow/tensorflow-${PKG_VERSION}-cp35-cp35m-manylinux1_x86_64.whl
-    elif [ "$PY_VER" == "3.6" ]; then
-        WHL_FILE=https://pypi.org/packages/cp36/t/tensorflow/tensorflow-${PKG_VERSION}-cp36-cp36m-manylinux1_x86_64.whl
+        WHL_FILE=https://github.com/lhelontra/tensorflow-on-arm/releases/download/v${PKG_VERSION}/tensorflow-${PKG_VERSION}-cp35-none-linux_armv7l.whl
     fi
 fi
 
